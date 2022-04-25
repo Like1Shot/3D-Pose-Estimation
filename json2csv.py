0caaaee52838beb982f7ceacb1b96464320c1b65
Chanheum Park <chpark@Chanheums-MacBook-Pro.local>
2022-04-25 09:43:57 -0600

    absolute directory

--- json2csv.py
+++ json2csv.py
@@ -7,22 +7,23 @@ dataTypeList = ['TrainSet', 'TestSet', 'ValSet']
 
 for dataType in dataTypeList:
     for dir in os.listdir(directory + dataType):
-        for filename in os.listdir(directory + dataType + '/' + dir):
-            print(filename)
-            f = os.path.join(directory, filename)
-            if os.path.isfile(f):
-                print(f)
+        for env in os.listdir(directory + dataType + '/' + dir + 'cam_down/' + 'json/'):
+            for filename in os.listdir(directory + dataType + '/' + dir):
+                print(filename)
+                f = os.path.join(directory, filename)
+                if os.path.isfile(f):
+                    print(f)
 
-            with open(filename) as json_file:
-                json_data = json.load(json_file)
+                with open(filename) as json_file:
+                    json_data = json.load(json_file)
 
-                joint_data = json_data['joints']
+                    joint_data = json_data['joints']
 
-                data_file  = open('', 'w')
-                csv_writer = csv.writer (data_file)
+                    data_file  = open('', 'w')
+                    csv_writer = csv.writer (data_file)
 
-                for i in range(len(joint_data)):
-                    csv_writer.writerow(joint_data[i])
+                    for i in range(len(joint_data)):
+                        csv_writer.writerow(joint_data[i])
         
-                data_file.close()
+                    data_file.close()
 
