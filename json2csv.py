fbec86b6c8e8dce026c3471227a593e6a690ca58
Chanheum Park <chpark@Chanheums-MacBook-Pro.local>
2022-04-25 09:39:42 -0600

    fix

--- json2csv.py
+++ json2csv.py
@@ -6,22 +6,23 @@ directory = '/home/chpark/Documents/xR-EgoPose/data/Dataset/'
 dataTypeList = ['TrainSet', 'TestSet', 'ValSet']
 
 for dataType in dataTypeList:
-    for filename in os.listdir(directory + dataType):
-        print(filename)
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            print(f)
+    for dir in os.listdir(directory + dataType):
+        for filename in os.listdir(directory + dataType + '/' + dir):
+            print(filename)
+            f = os.path.join(directory, filename)
+            if os.path.isfile(f):
+                print(f)
 
-        with open(filename) as json_file:
-            json_data = json.load(json_file)
+            with open(filename) as json_file:
+                json_data = json.load(json_file)
 
-            joint_data = json_data['joints']
+                joint_data = json_data['joints']
 
-            data_file  = open('', 'w')
-            csv_writer = csv.writer (data_file)
+                data_file  = open('', 'w')
+                csv_writer = csv.writer (data_file)
 
-            for i in range(len(joint_data)):
-                csv_writer.writerow(joint_data[i])
+                for i in range(len(joint_data)):
+                    csv_writer.writerow(joint_data[i])
         
-            data_file.close()
+                data_file.close()
 
