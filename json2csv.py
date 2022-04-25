a4dbb0fd36edb12ba037bfbee4690d59a2579e55
Chanheum Park <chpark@Chanheums-MacBook-Pro.local>
2022-04-25 09:12:40 -0600

    test

--- json2csv.py
+++ json2csv.py
@@ -2,22 +2,25 @@ import os
 import json 
 import csv
 
-directory = 'files'
+directory = 'xR-Ego-Pose/data/Dataset/' 
+dataTypeList = ['TrainSet', 'TestSet', 'ValSet']
 
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    if os.path.isfile(f):
-        print(f)
+for dataType in dataTypeList:
+    for filename in os.listdir(directory + dataType):
+        print(filename)
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):
+            print(f)
 
-    with open(filename) as json_file:
-        json_data = json.load(json_file)
+        with open(filename) as json_file:
+            json_data = json.load(json_file)
 
-        joint_data = json_data['joints']
+            joint_data = json_data['joints']
 
-        data_file  = open('', 'w')
-        csv_writer = csv.writer (data_file)
+            data_file  = open('', 'w')
+            csv_writer = csv.writer (data_file)
 
-        for i in range(len(joint_data)):
-            csv_writer.writerow(joint_data[i])
+            for i in range(len(joint_data)):
+                csv_writer.writerow(joint_data[i])
         
-        data_file.close()
+            data_file.close()
